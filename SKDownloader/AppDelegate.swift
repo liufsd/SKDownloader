2e6990e30f9bf57c56e7f8fa4fca60649954df8c
liupeng <liupeng@hujiang.com>
2015-11-19 20:33:55 +0800

    Initial Commit

---
+++ SKDownloader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SKDownloader
+//
+//  Created by liupeng on 11/19/15.
+//  Copyright © 2015 liupeng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SKDownloader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SKDownloader
+//
+//  Created by liupeng on 11/19/15.
+//  Copyright © 2015 liupeng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
